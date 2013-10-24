ace363cf7d97356b064b223a1cf553330a7f20d7
liuzenglu <zenglu.liu@gmail.com>
2013-10-24 10:58:30 +0800

    fix

--- init-centos.sh
+++ init-centos.sh
@@ -7,3 +7,6 @@ yum install -y openssh-clients rsync wget man gdb sysstat unzip vim git
 rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
 rpm -Uvh http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
 rpm -Uvh http://mirrors.yun-idc.com/epel/6/i386/epel-release-6-8.noarch.rpm
+
+groupadd mos
+useradd -g mos mos
