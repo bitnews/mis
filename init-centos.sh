cfbce117a9ff9a858e03450656ea081d93547991
liuzenglu <zenglu.liu@gmail.com>
2013-10-24 11:00:37 +0800

    fix

--- init-centos.sh
+++ init-centos.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+groupadd mos
+useradd -g mos mos
+
 yum install -y autoconf make cmake gcc gcc-c++
 
 yum install -y openssh-clients rsync wget man gdb sysstat unzip vim git
@@ -8,5 +11,4 @@ rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
 rpm -Uvh http://packages.sw.be/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
 rpm -Uvh http://mirrors.yun-idc.com/epel/6/i386/epel-release-6-8.noarch.rpm
 
-groupadd mos
-useradd -g mos mos
+yum update
